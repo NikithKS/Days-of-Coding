008cea260e6526a32f76767ec3e3d3713408f772
NikithKS <nikithks007@gmail.com>
2020-07-12 12:19:55 +0530

    Partial- Doctor Chef

--- DrChef.py
+++ DrChef.py
@@ -1,17 +1,54 @@
+def reach(a, b):
+    print("Reach-",a, b, end= " ")
+    count = 0
+    if (a <= b):
+        count = 1
+    else:
+        while (a > b):
+            b = b << 1
+            count += 1
+    print(count)
+    return count
+
 test = int(input())
 
 for _ in range(test):
     inp = input().split()
     x = int(inp[1])
     n = int(inp[0])
-
-    # arr = list(map(int, input().split()))
-    arr = input().split()
-
-    pop = int(arr[0])
     days = 0
-    while(x < pop):
-        x = x << 1;
-        days += 1
 
-    print(days + n)
+    arr = list(map(int, input().split()))
+    arr.sort(reverse = True)
+
+    if(arr[0] <= x):
+        print(n)
+        continue
+
+
+
+    startAt = n
+    if ((arr[n - 1] < x)):
+        while(arr[startAt - 1] <= x):
+            startAt -= 1
+
+        # if(arr[startAt] == x):
+        #     arr[startAt] = x << 1
+        if((arr[startAt - 1] / 2) <= (arr[startAt] << 1)):
+            days = -1
+        while(startAt < n):
+            if((arr[startAt - 1]) <= (arr[startAt] << 1)):
+                startAt += 1
+                days = -1
+            else:
+                break
+
+    print("Start={0} Day={1}".format(startAt,days))
+    for i in range(startAt - 1):
+        days += reach(arr[i], arr[i + 1])
+    days += reach(arr[startAt - 1] , x)
+
+    days += n - startAt + 1
+
+
+    print(days)
