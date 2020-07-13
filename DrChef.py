d08dffadc487def9eeb6a691802018c1fc259b92
NikithKS <nikithks007@gmail.com>
2020-07-13 17:35:04 +0530

    Doctor Chef- Codechef July Long

--- DrChef.py
+++ DrChef.py
@@ -1,17 +1,52 @@
-def reach(a, b):
-    # print("Reach-",a, b, end= " ")
-    count = 0
-    if (a >= b):
-        count = 1
-    else:
-        b2 = b
-        while (b != 0):
-            a = a << 1
-            b = min(max(0, (b - a)) << 1, b2)
-            count += 1
-    # print(count)
-    return count
+"""
+Doctor Chef
 
+Chef is multi-talented. He has developed a cure for coronavirus called COVAC-19. Now that everyone in the world is infected, it is time to distribute it throughout the world efficiently to wipe out coronavirus from the Earth. Chef just cooks the cure, you are his distribution manager.
+
+In the world, there are N
+countries (numbered 1 through N) with populations a1,a2,…,aN. Each cure can be used to cure one infected person once. Due to lockdown rules, you may only deliver cures to one country per day, but you may choose that country arbitrarily and independently on each day. Days are numbered by positive integers. On day 1, Chef has x
+
+cures ready. On each subsequent day, Chef can supply twice the number of cures that were delivered (i.e. people that were cured) on the previous day. Chef cannot supply leftovers from the previous or any earlier day, as the cures expire in a day. The number of cures delivered to some country on some day cannot exceed the number of infected people it currently has, either.
+
+However, coronavirus is not giving up so easily. It can infect a cured person that comes in contact with an infected person again ― formally, it means that the number of infected people in a country doubles at the end of each day, i.e. after the cures for this day are used (obviously up to the population of that country).
+
+Find the minimum number of days needed to make the world corona-free.
+
+
+Input:
+    The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains two space-separated integers N and x.
+The second line contains N space-separated integers a1,a2,…,aN.
+
+Output:
+    For each test case, print a single line containing one integer ― the minimum number of days.
+Constraints
+
+    1≤T≤103
+    1≤N≤105
+    1≤ai≤109 for each valid
+    i1≤x≤109
+the sum of N over all test cases does not exceed 106
+
+
+Example Input
+    3
+    5 5
+    1 2 3 4 5
+    5 1
+    40 30 20 10 50
+    3 10
+    20 1 110
+
+Example Output
+
+    5
+    9
+    6
+
+
+
+"""
 test = int(input())
 
 for _ in range(test):
@@ -21,63 +56,29 @@ for _ in range(test):
     days = 0
 
     arr = list(map(int, input().split()))
-    arr.append(x)
-    arr.sort()
 
-    if(arr[n] <= x):
+    arr.sort()
+    if(arr[n - 1] <= x):
         print(n)
         continue
 
     days = 0
-    ind = arr.index(x)
-    for i in range(ind + 1, n):
-        days += reach(arr[i], arr[i + 1])
+    idx = 0
 
-    if(ind != 0):
-        days += min(reach(arr[ind - 1], arr[ind + 1]), reach(x, arr[ind + 1]) + 1)
-    else:
-        days += reach(x, arr[1]) + 1
+    while (idx < n):
+        if (x < arr[idx]):
+            days += 1
+            x = x << 1
+        elif ((arr[idx] << 1) >= x):
+            days += 1
+            x = arr[idx] << 1
+            arr[idx] = 0
+            idx += 1
+        else:
+            idx += 1
 
-    if(n == 1):
-        days
-    days += ind
-
-    print(days)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-    if ((arr[n - 1] < x)):
-        while(arr[startAt - 1] <= x):
-            startAt -= 1
-
-        if((arr[startAt - 1] / 2) <= (arr[startAt] << 1)):
-            days = -1
-        while(startAt < n):
-            if((arr[startAt - 1]) <= (arr[startAt] << 1)):
-                startAt += 1
-                days = -1
-            else:
-                break
-
-    print("Start={0} Day={1}".format(startAt,days))
-    for i in range(startAt - 1):
-        days += reach(arr[i], arr[i + 1])
-    days += reach(arr[startAt - 1] , x)
-
-    days += n - startAt + 1
-
+    for i in range (n):
+        if(arr[i] > 0):
+            days += 1
 
     print(days)
-"""
